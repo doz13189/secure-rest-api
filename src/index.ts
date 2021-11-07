4bf175b1f693c670e0d3e3159ba5cbc3cac517f6
Takahiro Kato <dozo13189@gmail.com>
2021-11-07 14:40:15 +0900

    基本的なAPI部分の実装

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,21 @@
  * Required External Modules
  */
 
- import * as dotenv from "dotenv";
- import express from "express";
- import cors from "cors";
- import helmet from "helmet";
- 
- dotenv.config();
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { itemsRouter } from "./items/items.router";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
+
+dotenv.config();
 
 /**
- * App Variables
- */
+* App Variables
+*/
 
- if (!process.env.PORT) {
+if (!process.env.PORT) {
   process.exit(1);
 }
 
@@ -22,17 +25,21 @@ const PORT: number = parseInt(process.env.PORT as string, 10);
 const app = express();
 
 /**
- *  App Configuration
- */
+*  App Configuration
+*/
 
- app.use(helmet());
- app.use(cors());
- app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use("/api/menu/items", itemsRouter);
+
+// app.use(errorHandler);
+// app.use(notFoundHandler);
 
 /**
  * Server Activation
  */
 
- app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
